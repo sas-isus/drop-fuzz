92b489e025577e2aa70cc8f1fbae479ae0578f43
Brian Jopling <brj33@drexel.edu>
2017-08-14 10:11:42 -0400

    Add sample usage of ZAP API.

---
+++ zaproxy-example.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import time
+from pprint import pprint
+from zapv2 import ZAPv2
+
+target = 'http://127.0.0.1'
+
+# Get an API key by opening ZAP, going to Tools -> Options, selecting "API", and copying the key on the right.
+apikey = 'changeme' # Change to match the API key set in ZAP, or use None if the API key is disabled
+
+# By default ZAP API client will connect to port 8080
+zap = ZAPv2(apikey=apikey)
+# Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
+# zap = ZAPv2(apikey=apikey, proxies={'http': 'http://127.0.0.1:8090', 'https': 'http://127.0.0.1:8090'})
+
+# do stuff
+print 'Accessing target %s' % target
+# try have a unique enough session...
+zap.urlopen(target)
+# Give the sites tree a chance to get updated
+time.sleep(2)
+
+print 'Spidering target %s' % target
+scanid = zap.spider.scan(target)
+# Give the Spider a chance to start
+time.sleep(2)
+while (int(zap.spider.status(scanid)) < 100):
+    print 'Spider progress %: ' + zap.spider.status(scanid)
+    time.sleep(2)
+
+print 'Spider completed'
+# Give the passive scanner a chance to finish
+time.sleep(5)
+
+print 'Scanning target %s' % target
+scanid = zap.ascan.scan(target)
+while (int(zap.ascan.status(scanid)) < 100):
+    print 'Scan progress %: ' + zap.ascan.status(scanid)
+    time.sleep(5)
+
+print 'Scan completed'
+
+# Report the results
+
+print 'Hosts: ' + ', '.join(zap.core.hosts)
+print 'Alerts: '
+pprint (zap.core.alerts())
